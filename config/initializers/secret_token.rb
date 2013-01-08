ee9929339cba61bfedaf435ee34da8814eee0ee6
freakenbloopie <SF_Omega@yahoo.com>
2013-01-08 09:09:06 -0800

    Created a Users resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0da6d37269c6f1c87c7d62101a0be106eabe31c3e5fd35f9bae1b2b3ec590e3910ad48ce5b875667ea2d6d8fddd6abe767442b7c2e1a3fb69cd8b12086af5a9c'
